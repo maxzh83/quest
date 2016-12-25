d498d56261a2321cd460b29d46a8c2c2a9b6728b
maxzh83 <maxzh83@gmail.com>
2016-12-25 23:57:59 +0300

    q3

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,29 +2,44 @@
  * Created by Maxim on 25.12.2016.
  */
 angular.module('wizard-sample', ['mgo-angular-wizard'])
-    .controller('WizardCtrl', function($scope, WizardHandler) {
+    .controller('WizardCtrl', function ($scope, WizardHandler) {
 
-        $scope.pass = {pass1 : '123'};
+        $scope.pass = {
+            pass1: '',
+            pass2: '',
+            pass3: '',
+            pass4: '',
+            pass5: '',
+            pass6: ''
 
-        console.log("Started");
+        };
 
-        $scope.pass1 = function() {
-            if ($scope.pass.pass1 == '124'){
+
+        $scope.check = function(param, val ){
+            if (param == val) {
                 WizardHandler.wizard().next();
-            } else{
-                alert("Неверный код!")
+            } else {
+                alert("Неверный код, внимательней будь!")
             }
         };
 
-        $scope.finished = function() {
+        $scope.pass1 = function () {
+            $scope.check($scope.pass.pass1, '489');
+        };
+
+        $scope.pass2 = function () {
+            $scope.check($scope.pass.pass2, '44');
+        };
+
+        $scope.finished = function () {
             alert("Wizard finished :)");
         };
 
-        $scope.logStep = function() {
+        $scope.logStep = function () {
             console.log("Step continued");
         };
 
-        $scope.goBack = function() {
+        $scope.goBack = function () {
             WizardHandler.wizard().goTo(0);
         }
     });
